3a00106c116e8a3944bb1ff3e8eda8e35f0dd8b7
zhangrui <393394595@qq.com>
2022-09-04 15:01:47 +0800

    fix: 完善htmlwebpackplugin插入自定义标签的功能；

--- plugins/htmlWebpackInsertAsset.ts
+++ plugins/htmlWebpackInsertAsset.ts
@@ -1,25 +1,49 @@
 import HtmlWebpackPlugins from 'html-webpack-plugin'
 import { Compiler, Compilation } from "webpack";
 
-export type Options = {
-  src: string
+export type Options = Array<{
+  tag: 'meta' | 'script' | 'link'
+  attributes: {
+    src: string
+  } & Record<string, any>
+  /** 插入的位置，默认为after */
+  position?: 'before' | 'after'
+}>
+
+export type TagToAssetTagsMap = {
+  script: 'scripts'
+  link: 'styles'
+  meta: 'meta'
 }
 
 class HtmlWebpackInsertAsset {
-  private src = ''
+  options: Options = []
   constructor (options: Options) {
-    const { src } = options || {}
-    if(!src) {
-      throw Error('The src option is required')
-    }
-    this.src = src
+    this.options = options
+  }
+  private tagToAssetTagsMap: TagToAssetTagsMap = {
+    script: 'scripts',
+    link: 'styles',
+    meta: 'meta'
   }
   apply (compiler: Compiler) {
     compiler.hooks.compilation.tap('HtmlWebpackInsertAsset', (compilation: Compilation) => {
-      HtmlWebpackPlugins.getHooks(compilation).beforeAssetTagGeneration.tapAsync('HtmlWebpackInsertAsset', (data, cb) => {
-        const { assets } = data
-        const { js } = assets
-        js.push(this.src)
+      const htmlWebpackHooks = HtmlWebpackPlugins.getHooks(compilation)
+      htmlWebpackHooks.alterAssetTags.tapAsync('HtmlWebpackInsertAsset', (data, cb) => {
+        const { assetTags } = data
+        this.options.forEach(optionItem => {
+          const { tag, attributes, position = 'after' } = optionItem
+          const assetTag = this.tagToAssetTagsMap[tag]
+          const currentTags = assetTags[assetTag]
+          currentTags[position === 'after' ? 'push' : 'unshift']({
+            tagName: tag,
+            voidTag: false,
+            attributes,
+            meta: currentTags[0]?.meta || {
+              plugin: 'html-webpack-plugin'
+            }
+          })
+        })
         cb(null, data)
       })
     })
