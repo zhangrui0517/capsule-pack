6a6489bcb379dbf473c4359b6bdb4ea393048110
zhangrui <393394595@qq.com>
2022-09-05 23:05:38 +0800

    feat: cpack新增webpack构建命令；

--- src/command/webpackCommand.ts
+++ src/command/webpackCommand.ts
@@ -1,17 +1,48 @@
-import webpack from 'webpack'
+import Webpack from 'webpack'
+import WebpackDevServer from 'webpack-dev-server'
 import { Command } from 'commander'
-import dev from '../config/webpack.dev'
+import devConfig from '../config/webpack.dev'
+import prodConfig from '../config/webpack.prod'
 
 function webpackCommand(program: Command) {
   program
     .command('dev')
+    .description('构建开发环境产物')
     .action(() => {
-      webpack(dev(), (err, stats) => { // [Stats Object](#stats-object)
-        if (err || stats?.hasErrors()) {
-          console.log('stats: ',stats)
-          // [在这里处理错误](#error-handling)
+      Webpack(devConfig(), (err, stats) => {
+        if(stats) {
+          if (err || stats.hasErrors()) {
+          }
+          console.log(stats.toString())
         }
-        // 处理完成
+      })
+    })
+    program
+    .command('dev-server')
+    .description('构建开发环境产物，并启动本地服务')
+    .action(() => {
+      const webpackConfig = devConfig()
+      const compiler = Webpack(webpackConfig)
+      const devServerOptions = { ...webpackConfig.devServer }
+      const server = new WebpackDevServer(devServerOptions, compiler)
+      server.startCallback(() => {
+        console.log('Successfully started')
+      })
+      server.stopCallback(() => {
+        console.log('Successfully stop')
+      })
+      server.start()
+    })
+    program
+    .command('build')
+    .description('构建生产环境产物')
+    .action(() => {
+      Webpack(prodConfig(), (err, stats) => {
+        if(stats) {
+          if (err || stats.hasErrors()) {
+          }
+          console.log(stats.toString())
+        }
       })
     })
 }
