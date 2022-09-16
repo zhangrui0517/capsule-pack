453d4c456f14cd1b392d0b1f76775cacf8f1b6fe
zhangrui0517 <393394595@qq.com>
2022-09-16 11:19:24 +0800

    fix: 调整项目根路径，适配全局安装脚手架的情况；

--- src/command/path.ts
+++ src/command/path.ts
@@ -1,19 +1,16 @@
 export const child_process = require('child_process')
 export const path = require('path')
 // 当前项目路径
-export const rootPath = path.resolve(child_process.execSync('npm root').toString(), '../')
+export const rootPath = path.resolve(__dirname.split('node_modules')[0])
 // 当前命令执行路径
 export const cwdPath = process.cwd()
 
 // 当前的执行环境下的package.json
-export const packageJson = `${rootPath}/package.json`
+export const packageJson = `${cwdPath}/package.json`
 
 // 当前执行环境下的template文件夹路径
-export const currentTemplatePath = `${rootPath}/template`
+export const currentTemplatePath = `${cwdPath}/template`
 
 // capsule-pack 的相关路径
 export const cPackPath = `${rootPath}/node_modules/capsule-pack/src`
 export const cTemplatePath = `${cPackPath}/template`
-
-/** 支持的项目创建类型 */
-export const supportCreateType = ['react', 'tools', 'components']
