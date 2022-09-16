0026cfa7164968f29676ee5a4e07534d7e53a31b
zhangrui <393394595@qq.com>
2022-09-17 00:33:24 +0800

    chore: 将模板添加到构建产物包中

---
+++ lib/template/src/template/react/src/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * React 模板，提供React相关的打包支持，可直接启动开发
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export interface IProps {
+
+}
+
+const App = (props: IProps) => {
+  return (
+    <div>Hello capsule</div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
