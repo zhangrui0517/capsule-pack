c2c273a8cb456eef9a594e3bc2bfd41c2cbe6d2e
zhangrui <393394595@qq.com>
2022-09-21 14:03:02 +0800

    chore: 重新构建

--- lib/utils/path.d.ts
+++ lib/utils/path.d.ts
@@ -1,8 +1,8 @@
 export declare const projectPath: string;
-export declare const cacheDirPath: string;
-export declare const outputDirPath: string;
 export declare const packagePath: string;
-export declare const packageJson: string;
-export declare const currentTemplatePath: string;
-export declare const cPackPath: string;
-export declare const cTemplatePath: string;
+export declare function getCacheDirPath(): string;
+export declare function getOutputDirPath(): string;
+export declare function getPackageJson(): string;
+export declare function getCurrentTemplatePath(): string;
+export declare function getCpackPath(): string;
+export declare function getCtemplatePath(): string;
