aa02304440300b5c15305518778e6d03f1cf0410
zhangrui0517 <393394595@qq.com>
2022-09-26 23:40:03 +0800

    chore: 补全自定义webpack文件；

--- template/react/webpack.custom.js
+++ template/react/webpack.custom.js
@@ -3,6 +3,8 @@ const config = {
   // root: 'src',
   /** 是否需要动态polyfill注入 */
   // dynamicPolyfill: true
+  /** 是否启用babel对react的解析 */
+  react: true,
   /** webpack config 自定义调整，会通过webpack.merge与预设webpack配置合并 */
   // config: (webpackConfig) => webpackConfig
 }
