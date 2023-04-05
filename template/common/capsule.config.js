69e351706e44a7a59b7032dacc69a2073f4b6933
zhangrui0517 <393394595@qq.com>
2023-04-05 23:52:15 +0800

    feat: add common template

---
+++ template/common/capsule.config.js
@@ -0,0 +1,19 @@
+export default {
+	inquirer: [
+		{
+			name: 'react-template',
+			message: '模块名称：',
+			type: 'input'
+		},
+		{
+			name: 'zhangrui0517',
+			message: '作者：',
+			type: 'input'
+		},
+		{
+			name: 'react development template',
+			message: '描述：',
+			type: 'input'
+		}
+	]
+}
